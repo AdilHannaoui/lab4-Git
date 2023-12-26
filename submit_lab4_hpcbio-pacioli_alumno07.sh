fcbb6a3c553e7d18c19e985b8fe7be5491f8397b
AdilHannaoui <adilhannaoui4@gmail.com>
2023-12-26 21:36:01 +0100

    scripts del lab4

---
+++ submit_lab4_hpcbio-pacioli_alumno07.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -p hpc-bio-pacioli
+#SBATCH --chdir=/home/alumno07/
+#SBATCH -J par-lab2
+#SBATCH --ntasks-per-node=4  # a maximum count of tasks per node
+#SBATCH --cpus-per-task=4    # number of processors per task
+#SBATCH --mail-type=NONE    #END/START/NONE
+#SBATCH --mail-user=MAIL@um.es
+
+chmod +x ./file-cut.sh
+time srun -n 4 ./file-cut.sh
